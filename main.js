3a51d3ca97b9417c7df161794b1ef4742d593f88
DamianDamian-Domin <damian.domin334@gmail.com>
2022-04-03 18:16:14 +0200

    https to http

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ function geoFindMe() {
     
         var map = new L.map('map', mapOptions);
     
-        var layer = new L.TileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
+        var layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
     
         map.addLayer(layer);
     }
