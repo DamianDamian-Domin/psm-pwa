e253ae140d1509a22721a74ecd5c9f0728cb7bc5
DamianDamian-Domin <damian.domin334@gmail.com>
2022-04-03 17:19:54 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,35 @@
+function changeWindow(page) {
+    window.location.href = page
+}
+
+function geoFindMe() {
+
+
+    function success(position) {
+        const latitude  = position.coords.latitude;
+        const longitude = position.coords.longitude;
+
+        var mapOptions = {
+            center: [latitude, longitude],
+            zoom: 17
+        }
+    
+        var map = new L.map('map', mapOptions);
+    
+        var layer = new L.TileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
+    
+        map.addLayer(layer);
+    }
+
+    function error() {
+        status.textContent = 'Unable to retrieve your location';
+    }
+
+    if(!navigator.geolocation) {
+        status.textContent = 'Geolocation is not supported by your browser';
+    } else {
+        status.textContent = 'Locating…';
+        navigator.geolocation.getCurrentPosition(success, error);
+    }
+
+}
